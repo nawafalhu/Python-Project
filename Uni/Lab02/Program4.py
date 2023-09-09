95005718420f2d3784eec8ae5bd29285d042c90c
nawaf <n.alhu10@gmail.com>
2023-09-09 18:59:01 +0300

    added new file and fix some error

--- Uni/Lab02/Program4.py
+++ Uni/Lab02/Program4.py
@@ -1,13 +1,14 @@
-string1 = input("Enter the first string: ").split()
-string2 = input("Enter the second string: ").split()
+str1 = input("Enter the first string: ").upper()
+str2 = input("Enter the second string: ").upper()
 
 
+def check(string1, string2) :
+    string1 = sorted(string1)
+    string2 = sorted(string2)
 
-def same_char(First_string, second_string) : 
-    First_string.sort()
-    second_string.sort()
-    for n in First_string, second_string :
-        print(First_string)
-        print(second_string)
+    for n in range(len(string1)) :
+        if string1[n] != string2[n] : 
+            return False
+    return True
 
-same_char(string1, string2)
+print(check(str1, str2))
--- Uni/Lab02/Program5.py
+++ Uni/Lab02/Program5.py
@@ -4,6 +4,5 @@ special_char = '@_!#$%^&*()<>?/\|}{~:;.[]'
  
 out_list = [''.join(x for x in string if not x in special_char) for string in list]
 
-print(out_list)
-
 print(list)
+print(out_list)
